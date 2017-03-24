7de3ea50fc1fc412823bf928fa9b287cc79dd9c0
teg-github <sakthi.kailasam@tcs.com>
2017-03-24 11:54:01 +0530

    Update copy-code.sh

--- build-scripts/copy-code.sh
+++ build-scripts/copy-code.sh
@@ -4,8 +4,8 @@ echo Details --- $UPSELL_ENABLED
 if [ $UPSELL_ENABLED -eq 1 ];
 then
   echo "Its 1"
-  cp -rf ./Build-Configurations/enabled/ActorChainRestRegistry.properties ./MicroServices/config/atg/rest/registry/ActorChainRestRegistry.properties
+  cp -rf ./Build-Configurations/enabled/ActorChainRestRegistry.properties .
 else
   echo "Its 0"
-  cp -rf ./Build-Configurations/disabled/ActorChainRestRegistry.properties ./MicroServices/config/atg/rest/registry/ActorChainRestRegistry.properties
+  cp -rf ./Build-Configurations/disabled/ActorChainRestRegistry.properties .
 fi
