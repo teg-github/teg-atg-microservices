573789f9713b69071ae52106ecbece273e41eedb
teg-github <sakthi.kailasam@tcs.com>
2017-03-24 16:03:45 +0530

    Update sonar-analysis.bat

--- build-scripts/sonar-analysis.bat
+++ build-scripts/sonar-analysis.bat
@@ -1,2 +1,1 @@
-cd C:\sonar-scanner-2.5.1\bin\
-sonar-runner -Dsonar.projectKey=WW-ATG-MicroService-Report -Dsonar.projectName=WW-ATG-MicroService-Report -Dsonar.projectVersion=1.0 -Dsonar.sources=./MicroServices/src
+C:\sonar-scanner-2.5.1\bin\sonar-runner -Dsonar.projectKey=WW-ATG-MicroService-Report -Dsonar.projectName=WW-ATG-MicroService-Report -Dsonar.projectVersion=1.0 -Dsonar.sources=./MicroServices/src
