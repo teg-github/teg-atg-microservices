9c175fcc4b01884c7e40779970a0960158141b91
teg-github <sakthi.kailasam@tcs.com>
2017-03-24 11:33:20 +0530

    Create prepare-env.sh

---
+++ build-scripts/prepare-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if grep -Fxq "getUpsellForProduct" ../MicroServices/atg/rest/registry/ActorChainRegistry.properties
+then
+    echo UPSELL_ENABLED=1 > /env.properties
+else
+    echo UPSELL_ENABLED=0 > /env.properties
+fi
